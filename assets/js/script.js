a56ebd02bce1e44e7c30d3e528fbe6c60a4cc532
Payn <ivanttt54@gmail.com>
2023-03-19 13:39:10 -0300

    change the typewrite effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,8 @@
 var console = document.getElementById('hello-console');
 
-var typewriter = new Typewriter(console, {
-  loop: true,
-  delay: 100,
-  deleteSpeed: 100
-});
-
-typewriter.typeString('Hi my name is Payn, Im a backend and low level developer')
-    .pauseFor(2500)
-    .deleteAll()
-    .start();
+typer(console, 100)
+    .line("Hi, I am <strong>Payn</strong>", {totalTime: 850})
+    .back('all')
+    .continue("Hi, I am <strong>Payn</strong>", {totalTime: 850})
+    .back('all')    
+    .repeat(Infinity, true);
